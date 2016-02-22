51add48d474b4e20507df2979a3c4bb5016ea35e
Tune <Reidcrawford55@gmail.com>
2016-02-22 11:30:16 -0800

    this is a test for heroku app

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,25 @@
 Rails.application.routes.draw do
+  get 'users/index'
+
+  get 'users/create'
+
+  get 'users/new'
+
+  get 'users/edit'
+
+  get 'users/show'
+
+  get 'users/update'
+
+  get 'users/destroy'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
+
+  resources :routes
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
